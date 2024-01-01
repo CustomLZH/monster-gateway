203c025329e399fe2f415093ea134d2aebfaaf2e
CustomLZH <2357316121@qq.com>
2024-01-02 00:11:53 +0800

    修改pom

--- src/main/java/com/monster/gateway/MonsterGatewayApplication.java
+++ src/main/java/com/monster/gateway/MonsterGatewayApplication.java
@@ -2,13 +2,11 @@ package com.monster.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author CustomLZH
  */
 @SpringBootApplication
-@EnableEurekaClient
 public class MonsterGatewayApplication {
 
     public static void main(String[] args) {
